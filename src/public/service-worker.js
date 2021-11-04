8dc543233308d7f054ec876cbf16a98e59bb7b29
ColinNebula <57843842+ColinNebula@users.noreply.github.com>
2021-11-04 15:43:34 -0400

    added horoku

--- src/public/service-worker.js
+++ src/public/service-worker.js
@@ -3,10 +3,10 @@ const FILES_TO_CACHE = [
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
     "/index.html",
-    "/index.js",
-    "/manifest.webmanifest",
-    "/styles.css",
-    "/db.js"
+    "/js/index.js",
+    "/js/manifest.webmanifest",
+    "/css/styles.css",
+    "/js/idb.js"
 ];
 
 const CACHE_NAME = "static-cache-v2";

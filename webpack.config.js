7842eb4d1a2a78aef4c75ba7c8abcc31851bc11b
ColinNebula <57843842+ColinNebula@users.noreply.github.com>
2021-11-04 15:07:49 -0400

    updated srever

--- webpack.config.js
+++ webpack.config.js
@@ -4,26 +4,39 @@ const webpack = require('webpack');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 const config = {
-    plugins: [
-new WebpackPwaManifest({
-    name: 'Budget Tracker',
-    short_name: 'Budget',
-    description: 'An app that allows you to track your budget.',
-    start_url: '../index.html',
-    background_color: '#01579b',
-    theme_color: '#ffffff',
-    fingerprints: false,
-    inject: false,
-    icons: [
+  module: {
+    rules: [
       {
-        src: path.resolve('public/icons/icon-512x512.png'),
-        sizes: [96, 128, 192, 256, 384, 512],
-        destination: path.join('assets', 'icons')
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+              loader: 'url-loader',
+          }
+        ]
       }
-    ]
-  })
-],
-mode: 'development'
+    ],
+  },
+  plugins: [
+    new WebpackPwaManifest({
+      name: 'Budget Tracker',
+      short_name: 'Budget',
+      description: 'An app that allows you to track your budget.',
+      start_url: './src/index.html',
+      background_color: '#01579b',
+      theme_color: '#ffffff',
+      fingerprints: false,
+      inject: false,
+      icons: [
+        // {
+        //   src: path.resolve('public/icons/icon-512x512.png'),
+        //   sizes: [96, 128, 192, 256, 384, 512],
+        //   destination: path.join('assets', 'icons')
+        // }
+      ]
+    })
+  ],
+  entry: "./src/server.js",
+  mode: 'development'
 };
 
 module.exports = config;

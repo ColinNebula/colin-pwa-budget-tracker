4e81b535ee35c6e5f31ca63f3b1fe5ff62078d10
ColinNebula <57843842+ColinNebula@users.noreply.github.com>
2021-11-04 15:46:43 -0400

    added horoku

--- src/public/service-worker.js
+++ src/public/service-worker.js
@@ -3,8 +3,8 @@ const FILES_TO_CACHE = [
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
     "/index.html",
-    "/js/index.js",
     "/js/manifest.webmanifest",
+    "/js/index.js",
     "/css/styles.css",
     "/js/idb.js"
 ];
